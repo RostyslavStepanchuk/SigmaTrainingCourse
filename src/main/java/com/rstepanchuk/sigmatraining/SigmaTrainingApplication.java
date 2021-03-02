349216fee94d3c5c1dcd84122af09bba625e0db1
Rostyslav Stepanchuk <rostyslav.stepanchuk@gmail.com>
2021-03-02 18:33:15 +0200

    Initialized Gradle Spring-Boot project

---
+++ src/main/java/com/rstepanchuk/sigmatraining/SigmaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.rstepanchuk.sigmatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaTrainingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SigmaTrainingApplication.class, args);
+  }
+
+}
